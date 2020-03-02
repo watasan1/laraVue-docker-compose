6dfc85e4ad6866af98764aa7842c1fa575d17998
watasan <ishiwatahirotaka1012@gmail.com>
2020-03-02 19:02:26 +0900

    rotes5-15

--- var/www/backend/routes/api.php
+++ var/www/backend/routes/api.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\Http\Resources\ContactResource;
+
+// 連絡先API
+Route::apiResource('contact','ContactController');
 
 /*
 |--------------------------------------------------------------------------
