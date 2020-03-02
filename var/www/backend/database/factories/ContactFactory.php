2fae626e08ce5d1d8aba196d61791c6ff88fef57
watasan <ishiwatahirotaka1012@gmail.com>
2020-03-02 14:24:56 +0900

    dammy5_11

--- var/www/backend/database/factories/ContactFactory.php
+++ var/www/backend/database/factories/ContactFactory.php
@@ -8,5 +8,18 @@ use Faker\Generator as Faker;
 $factory->define(Contact::class, function (Faker $faker) {
     return [
         //
+        'first_name' => $faker->firstName,
+        'last_name' => $faker->lastName,
+        'avatar' => 'https://avatars0.githubusercontent.com/u/9064066?v=4&s=460',
+        'gender' => function(){
+            return mt_rand(1,3);
+        },
+        'phone_number' => $faker->phoneNumber,
+        'house_phone_number' => '',
+        'email' => $faker->email,
+        'address' => $faker->streetAddress,
+        'birthday' => $faker->date($format = 'Y-m-d'),
+        'memo' => $faker->text,
+
     ];
 });
