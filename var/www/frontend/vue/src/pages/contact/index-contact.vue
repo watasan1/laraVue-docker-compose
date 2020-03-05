100af09e338f5bedcac5a2b0a254c128a6b1cd75
watasan <ishiwatahirotaka1012@gmail.com>
2020-03-05 16:22:09 +0900

    VueComponenttemplate6_12

---
+++ var/www/frontend/vue/src/pages/contact/index-contact.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>index-contact.vue</div>
+</template>
+
+<script>
+    export default {};
+</script>
---
+++ var/www/frontend/vue/src/pages/contact/save-contact.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>save-contact.vue</div>
+</template>
+
+<script>
+    export default{};
+</script>
