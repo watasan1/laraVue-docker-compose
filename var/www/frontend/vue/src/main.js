2145a7412671e677421a26529917c019717bc638
watasan <ishiwatahirotaka1012@gmail.com>
2020-03-06 12:19:32 +0900

    hederadd7/7

--- var/www/frontend/vue/src/main.js
+++ var/www/frontend/vue/src/main.js
@@ -2,9 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router' // 6/8 edit
 
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import '@mdi/font/css/materialdesignicons.css'
+
+Vue.use(Vuetify)
+
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
   router,  // 6/8 edit
+  vuetify: new Vuetify(), // 7/5add
 }).$mount('#app')
