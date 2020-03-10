c1fd20033138d24da7000d87360bcdb352102ede
watasan <ishiwatahirotaka1012@gmail.com>
2020-03-11 01:22:21 +0900

    APIから連絡先一覧データを取得する7/11

--- var/www/frontend/vue/src/pages/contact/index-contact.vue
+++ var/www/frontend/vue/src/pages/contact/index-contact.vue
@@ -5,10 +5,32 @@
 </template>
 
 <script>
-import Header from "../../components/Header";
+import Header from '../../components/Header'
+import axios from 'axios' // 7_11 add
+
+const BASE_URL = 'http://localhost:8000'
+
 export default {
   components: {
     Header
+  },
+  // 以下add
+  data: () => ({}),
+  methods: {
+    axiosGetContacts() {
+      let endpoint = BASE_URL + '/api/contact'
+      axios
+        .get(endpoint)
+        .then(res => {
+          console.log(res)
+        })
+        .catch(error => {
+          console.warn(error)
+        })
+    }
+  },
+  mounted() {
+    this.axiosGetContacts()
   }
-};
+}
 </script>
